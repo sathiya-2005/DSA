6cb150130932b08a223c28e7362c2218df82efab
sathiya-2005 <sxthiya.enquiries@gmail.com>
2024-06-03 20:16:36 +0530

    Some changes were made

--- Transpose.java
+++ Transpose.java
@@ -1,4 +1,4 @@
-public class sa {
+public class Transpose {
     public static void main(String args[]){
         int [][]arr={{1,2,3,4,},{5,6,7,8,},{9,10,11,12},{13,14,15,16}};
         for(int i =0;i<arr.length;i++){
